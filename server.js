f840f423806db7fa6a3eee65008a7be9eb6ae788
Alexander Mayer <alexander.mayer@struktur1.com>
2017-06-04 14:00:00 +0200

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use((request, response, next) => {
+  // openweathermap only works with http in the free version
+  // We need to check whether https:// is used and redirect then to the http://
+  if (request.headers['x-forwarded-proto'] === 'https') {
+    response.redirect('http://' + request.hostname + request.url);
+  } else {
+    next();
+  }
+});
 
 app.use(express.static('public'));
 
@@ -12,7 +23,6 @@ app.get('*', function (req, res, next) {
     res.sendFile(path.resolve('public/index.html'));
 });
 
-
-app.listen(3000, () => {
-  console.log('Express server is up on port 3000...');
+app.listen(PORT, () => {
+  console.log(`Express server is up on port ${PORT}...`);
 })
