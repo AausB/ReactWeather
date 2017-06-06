91342c916c4341ee1765a5bea2a1b2b7b408a708
Alexander Mayer <alexander.mayer@struktur1.com>
2017-06-06 11:32:34 +0200

    Fixed error Modal

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,6 +1,10 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 
+// because foundation does remove the modal from the DOM after closing:
+const ReactDOM = require('react-dom');
+const ReactDOMServer = require('react-dom/server');
+
 class ErrorModal extends React.Component {
   constructor(props) {
     super(props);
@@ -8,14 +12,12 @@ class ErrorModal extends React.Component {
   }
 
   componentDidMount = () => {
-    let modal = new Foundation.Reveal($('#error-modal'));
-    modal.open();
-  }
-
-  render() {
+    //
+    // lets get tricky
+    //
     let {title, message} = this.props;
 
-    return(
+    let modalMarkup = (
       <div id="error-modal" className="reveal tiny text-center" data-reveal="">
         <h4>{title}</h4>
         <p>{message}</p>
@@ -23,6 +25,22 @@ class ErrorModal extends React.Component {
           <button className="button hollow" data-close="">Okay</button>
         </p>
       </div>
+    );
+
+    // jQuery creates an element from HTML markup
+    let $modal = $(ReactDOMServer.renderToString(modalMarkup));
+    // add the created element into the React Modal Element
+    $(ReactDOM.findDOMNode(this)).html($modal);
+
+    // run the modal element
+    let modal = new Foundation.Reveal($('#error-modal'));
+    // Foundation makes some changes to the DOM
+    modal.open();
+  }
+
+  render() {
+    return (
+      <div></div>
     );
   }
 }
